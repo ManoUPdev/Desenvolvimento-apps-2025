96c3b419fa8c27b4f55ad73cd7c5b549d9d959e7
ManoUPdev <emanuelcgalindo@hotmail.com>
2025-09-17 09:58:36 -0300

    bruh

--- NubankCryptosTela/app/src/main/java/com/example/nubankcryptostela/MainActivity.kt
+++ NubankCryptosTela/app/src/main/java/com/example/nubankcryptostela/MainActivity.kt
@@ -3,27 +3,37 @@ package com.example.nubankcryptostela
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.nubankcryptostela.ui.theme.NubankCryptosTelaTheme
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+
+
+data class Crypto(val name: String, val price: Double, val variation: Double)
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NubankCryptosTelaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContent{
+            val navController = rememberNavController()
+            navHost(navController, startDestination = "login") {
+                composable("login") { LoginScreen(navController) }
+                composable("list")  { CryptoListScreen(navController) }
+                composable("detail/{crypto}") { backStackEntry ->
+                    val cryptoName = backStackEntry.arguments?.getString("crypto") ?: ""
+                    CryptoDetailScreen(navController, cryptoName)
+                }
+                composable("buySell/{crypto}") { backStackEntry ->
+                    val cryptoName = backStackEntry.arguments?.getString("crypto") ?: ""
+                    BuySellScreen(navController, cryptoName)
                 }
             }
         }
@@ -31,17 +41,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun loginScreen(navController: NavController){
+    val context = LocalContext.current
+    var email by remember { mutableStateOf(TextFieldValue(""))}
+    var password by remember { mutableStateOf(TextFieldValue(""))}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NubankCryptosTelaTheme {
-        Greeting("Android")
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
     }
 }
